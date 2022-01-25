3307fffcf947a40d3eb42f86660ba9cf405c3b80
Krupa0212 <81922378+Krupa0212@users.noreply.github.com>
2022-01-25 12:13:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 18;
+        int b = 6;
+        Pro2.branching(a, b);
+        Pro2.compare();
+        Pro2.compare2();
+        Pro2.ifElse();
+        Pro2.ifElse2();
+        Pro2.whileLoop();
+        Pro2.doWhileLoop();
+        Pro2.forLoop();
+        Pro2.nestedLoop();
+        Pro2.isDivsibleBy3();
+
+    }
+}
+
+
